9343b19c7000f8bf32b8553d17fdd01b45cbfde5
kirraObj <c@skymc.me>
2024-03-16 20:32:41 +0800

    update.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("org.freemarker:freemarker:2.3.32")
+    compileOnly("com.squareup.okhttp3:okhttp:4.12.0")
+    compileOnly("org.freemarker:freemarker:2.3.32")
 }
 
 
@@ -53,21 +53,6 @@ tasks {
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("222")
-        untilBuild.set("232.*")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
     }
 }
 
