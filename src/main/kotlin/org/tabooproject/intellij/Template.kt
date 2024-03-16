79afc2f0e3b010f7935e0ba174cd6cf3951933ac
kirraObj <c@skymc.me>
2024-03-16 16:14:01 +0800

    update

--- src/main/kotlin/org/tabooproject/intellij/Template.kt
+++ src/main/kotlin/org/tabooproject/intellij/Template.kt
@@ -37,54 +37,44 @@ object Template {
             .takeIf { it.isSuccessful } ?: throw IOException("Failed to download file")
 
         response.body?.byteStream()?.let { inputStream ->
-            unzipInMemory(inputStream).forEach { (path, stream) ->
-                process(path, stream, baseDir)
+            ZipInputStream(inputStream).use { zip ->
+                var entry = zip.nextEntry
+                while (entry != null) {
+                    if (!entry.isDirectory) {
+                        val entryName = entry.name.substringAfter("taboolib-sdk-idea-template/")
+                        if (entryName.endsWith(".ftl")) {
+                            val contentBytes = zip.readBytes()
+                            processEntry(entryName, contentBytes, baseDir)
+                        }
+                    }
+                    zip.closeEntry()
+                    entry = zip.nextEntry
+                }
             }
         } ?: throw IOException("Response body is null")
     }
 
-    private fun unzipInMemory(inputStream: InputStream): List<Pair<String, InputStream>> {
-        val fileList = mutableListOf<Pair<String, InputStream>>()
-
-        ZipInputStream(inputStream).use { zip ->
-            var entry = zip.nextEntry
-            while (entry != null) {
-                if (!entry.isDirectory) {
-                    val fileBytes = zip.readBytes()
-                    val entryName = entry.name.substringAfter("taboolib-sdk-idea-template/")
-                    fileList.add(entryName to ByteArrayInputStream(fileBytes))
-                }
-                zip.closeEntry()
-                entry = zip.nextEntry
-            }
-        }
-        return fileList
-    }
-
-    private fun process(path: String, stream: InputStream, baseDir: String) {
-        val rawContent = stream.readBytes().toString(StandardCharsets.UTF_8)
-        val replacedPath = path.replace(".ftl", "")
-        val templateFile = TEMPLATE_FILES[replacedPath] ?: return
-        val content = read(templateFile, rawContent)
-        val writtenPath = replacedPath
-            .replace("io.github/username/project/ExamplePlugin",
-                ConfigurationPropertiesStep.property.mainClass.replace(".", "/")
-            )
-        createFileWithDirectories(baseDir, writtenPath)?.also { file ->
-            Files.write(file, content.toByteArray(StandardCharsets.UTF_8))
-        }
-    }
-
-    private fun read(templateFile: TemplateFile, content: String): String {
+    private fun processEntry(entryName: String, contentBytes: ByteArray, baseDir: String) {
+        val path = entryName.replace(".ftl", "")
+        val templateFile = TEMPLATE_FILES[path] ?: return
         val data = FunctionTemplate.getBuildProperty()
+
         val cfg = Configuration(Configuration.VERSION_2_3_31).apply {
-            templateLoader = StringTemplateLoader()
+            templateLoader = StringTemplateLoader().also { it.putTemplate(templateFile.node, String(contentBytes, StandardCharsets.UTF_8)) }
             defaultEncoding = "UTF-8"
         }
-        val template = Template(templateFile.node, StringReader(content), cfg)
-        StringWriter().use { writer ->
-            template.process(data, writer)
-            return writer.toString()
+
+        val finalContent = StringWriter().use { writer ->
+            Template(templateFile.node, StringReader(String(contentBytes, StandardCharsets.UTF_8)), cfg).process(data, writer)
+            writer.toString()
+        }
+
+        val replacedPath = path.replace(
+            "io.github/username/project/ExamplePlugin",
+            ConfigurationPropertiesStep.property.mainClass.replace(".", "/")
+        )
+        createFileWithDirectories(baseDir, replacedPath)?.also { file ->
+            Files.write(file, finalContent.toByteArray(StandardCharsets.UTF_8))
         }
     }
 
